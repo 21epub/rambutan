ee9fed8babc82a25fe0727a3896fbdb9737e9bef
jiaxin <edison7500@gmail.com>
2020-10-09 16:34:16 +0800

    image prefix change to epub360-media from image

--- apps/views/images/__init__.py
+++ apps/views/images/__init__.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
 
-images = Blueprint("images", __name__, url_prefix="/image")
+images = Blueprint("images", __name__, url_prefix="/epub360-media")  # noqa
 
 from . import views
